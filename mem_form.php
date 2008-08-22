a26c956fce7286e7820b1e44bf56908f5a32c4b4
netcarver <nc.github@netcarving.com>
2008-08-23 00:31:37 +0800

    Updated Meta-data area for new compiler

--- mem_form.php
+++ mem_form.php
@@ -1,32 +1,15 @@
 <?php
 
-// This is a PLUGIN TEMPLATE.
-
-// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
-// run this file at the command line to produce a plugin for distribution:
-// $ php abc_myplugin.php > abc_myplugin-0.1.txt
-
-// Plugin name is optional.  If unset, it will be extracted from the current
-// file name. Uncomment and edit this line to override:
-# $plugin['name'] = 'abc_plugin';
-
-// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
-// 1 = Plugin help is in raw HTML.  Not recommended.
+$plugin['name'] = 'mem_form';
 # $plugin['allow_html_help'] = 1;
 // $Rev: 99 $ $LastChangedDate: 2008-05-17 11:40:19 +0800 (Sat, 17 May 2008) $
 $plugin['version'] = '0.4.1';
 $plugin['author'] = 'Michael Manfre';
 $plugin['author_uri'] = 'http://manfre.net/';
 $plugin['description'] = 'A library plugin that provides support for html forms.';
-
-// Plugin types:
-// 0 = regular plugin; loaded on the public web side only
-// 1 = admin plugin; loaded on both the public and admin side
-// 2 = library; loaded only when include_plugin() or require_plugin() is called
 $plugin['type'] = 2; 
 
-if (!defined('txpinterface'))
-	@include_once('../zem_tpl.php');
+if (!defined('txpinterface')) @include_once('../zem_tpl.php');
 
 if (0) {
 ?>
